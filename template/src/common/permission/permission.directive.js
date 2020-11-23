e38241cda69b1283b11bfdba66999e0e35423ca7
白惠波 <baihuibo@5i5j.com>
2020-11-23 16:06:32 +0800

    v1.0.16 调整了权限代码，新增了更细颗粒度的缓存策略

--- template/src/common/permission/permission.directive.js
+++ template/src/common/permission/permission.directive.js
@@ -11,20 +11,19 @@ export default {
 }
 
 function doPermission(el, arg, show) {
-    if (el[arg]) {
-        setStyle();
-        return;
-    }
-    el[arg] = true;
-    findPermission(arg, setStyle);
+    const matchs = arg.match(PermissionExecReg) || []; // 找到所有的权限
 
-    function setStyle(map = cache) {
+    // 查询权限编码，然后设置样式
+    findPermission(matchs, setStyle);
+
+    function setStyle() {
         // 开发环境时，默认所有权限都为true
         // 非开发环境时，需要根据权限调整元素显示
         if (process.env.NODE_ENV === 'development') {
             return;
         }
-        if (map[arg] === false && el.style) {
+
+        if (!hasPermission(matchs) && el.style) {
             el.tabIndex = -1;
             el.style.pointerEvents = 'none';
             el.style.color = '#c1c1c1';
@@ -34,32 +33,91 @@ function doPermission(el, arg, show) {
             }
         }
     }
+
+    /**
+     * 将表达式构建成一个函数
+     * @example
+     *  arg = 'AAA:BBB'
+     *  buildFn() => 'function fn(){ return true }'
+     * @example
+     *  arg = 'AAA:BBB || BBB:CC && AA:CC && !DD:FA'
+     *  buildFn() => 'function fn(){ return true || true && false && !false }'
+     *  @param matchs {String[]}
+     * @return {Function}
+     */
+    function hasPermission(matchs) {
+        let fn = calcFnCache[arg];
+        if (!fn) {
+            let fnBody = arg;
+            matchs.forEach(e => {
+                fnBody = fnBody.replace(new RegExp(e, 'g'), PermissionCacheMap[e]);
+            });
+            fn = calcFnCache[arg] = Function(`return !!(${fnBody})`);
+        }
+        return fn();
+    }
 }
 
 // 查询权限
-let codes = new Set(), callbacks = [], cache = {}, timer;
+let codeQueues = new Set(), // 查询队列
+    lazeCodeQueues = new Set(), // 懒查询队列(也就是不触发真正的查询)
+    callbackQueues = [], // 回调函数队列
+    PermissionExecReg = /(?:\w+:?)+/g,
+    PermissionCacheMap = {}, // 权限数据缓存
+    calcFnCache = {}, // 计算权限函数缓存
+    requestCache = {}, // 权限查询对象缓存
+    timer;
 
-function findPermission(code, cb) {
-    if (typeof cache[code] !== 'undefined') { // 查询缓存
-        return cb(cache);
+function findPermission(matchCodes, cb) {
+    // 过滤缓存中已经存在的权限
+    matchCodes = matchCodes.filter(e => PermissionCacheMap[e] == null)
+
+    // 若全部权限都存在缓存，则可以直接触发回调即可
+    if (!matchCodes.length) {
+        return cb();
     }
 
-    codes.add(code);
-    callbacks.push(cb);
+    // 将剩余的权限放入查询队列
+    matchCodes.forEach(key => {
+        if (!requestCache[key]) {
+            // 还未查询过的key，放到codes中,将在下一次查询时，处理它
+            codeQueues.add(key);
+        } else {
+            // 若已经查询过的key，不会触发新的查询，而是复用已查询过的promise缓存
+            lazeCodeQueues.add(key);
+        }
+    });
+    callbackQueues.push(cb); // 但是无论是否查询，回调函数总是需要触发
 
     if (timer) return;
 
     timer = 1;
-    nextTick(async function () {
+    nextTick(async () => {
         timer = 0;
-        const searchCodes = Array.from(codes).join(',');
-        const cbs = callbacks.slice(0);
-        codes.clear();
-        callbacks.length = 0;
-        const result = await post('/bdc/search/checkPermission.htm', {code: searchCodes});
-        const map = result.data || {};
-        Object.assign(cache, map); // 缓存数据
-        cbs.forEach(fn => fn(map));
+        const searchCodes = Array.from(codeQueues);
+        const lazeCodes = Array.from(lazeCodeQueues);
+        const cbFns = callbackQueues.slice(0);
+
+        codeQueues.clear();
+        lazeCodeQueues.clear();
+        callbackQueues.length = 0;
+
+        // 复用已经查询过的promise
+        const requests = lazeCodes.map(code => requestCache[code]);
+
+        // 查询未查询过的权限
+        if (searchCodes.length) {
+            const queryBody = {code: searchCodes.join(',')};
+            const promise = post('/bdc/search/checkPermission.htm', queryBody).then(result => {
+                Object.assign(PermissionCacheMap, result.data || {}); // 缓存数据
+            });
+            searchCodes.forEach(code => requestCache[code] = promise); // 缓存 promise
+            requests.push(promise);
+        }
+
+        await Promise.all(requests);
+
+        cbFns.forEach(fn => fn());
+        cbFns.length = 0;
     });
 }
-
--- template/src/common/permission/readme.md
+++ template/src/common/permission/readme.md
@@ -13,7 +13,35 @@
                 v-permission:PERF:RULE.show>功能按钮</button>
     </form>
 </template>
+```
 
+进阶用法，支持与或非，多权限
+```vue
+<template>
+    <form>
+        (PERF:RULE1) || ( PERF:RULE2 && PERF:RULE3 )  下面俩种写法是一样的（括号可选）
+        <button type="button"
+                v-permission:(PERF:RULE1)||(PERF:RULE2&&PERF:RULE3)>功能按钮</button>
+        <button type="button"
+                v-permission:PERF:RULE1||(PERF:RULE2&&PERF:RULE3)>功能按钮</button>
+
+
+        (PERF:RULE2) && (PERF:RULE1)  下面俩种写法效果一样（括号可选）
+        <button type="button"
+                v-permission:(PERF:RULE2)&&(PERF:RULE1).show>功能按钮</button>
+        <button type="button"
+                v-permission:PERF:RULE2&&PERF:RULE1.show>功能按钮</button>
+
+        因为语法限制，上面的 && || ! 与或非符号中间，不允许添加空格，还可以这样写改为使用模板变量
+        <button type="button"
+                v-permission:[mutlPermission]>功能按钮</button>
+        <button type="button"
+                v-permission:[mutlPermission].show>功能按钮</button>
+    </form>
+</template>
+<script >
+export const mutlPermission = 'RULE1 || rule2 || (rule3 && rule4)';
+</script>
 ```
 
  - 可在任意html标签上使用
